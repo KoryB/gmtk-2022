92d639d43965f0d2d66ce4bb7341205dcb772ed9
Kory Byrne <byrnekory@gmail.com>
2022-07-16 16:13:33 -0400

    Added some nicer controls, killing dice

--- DiceCop.cs
+++ DiceCop.cs
@@ -6,67 +6,133 @@ public class DiceCop : RigidBody2D
     [Export] 
     private int _slowdownModulo = 2;
 
+    [Export]
+    public float JumpStrength = 100.0f;
+    
+    [Export]
+    public float JumpRotation = 400.0f;
+
+    [Export]
+    public Vector2 JumpAngleBounds = new Vector2(Mathf.Pi / 6.0f, Mathf.Pi / 2.0f);
+    
+    [Export]
+    public float JumpAngleChangeDuration = 2.0f;
+
+
+    public bool IsChargingJump => _isChargingJump;
+
 
     private enum Direction
     {
         Left = -1,
-        Right = 1
+        Right = 1,
+        None = 0
     }
 
     private bool _isSlowdown = false;
     private int _slowdownFrameCount = 0;
 
+    private bool _isChargingJump = false;
+    private float _jumpChargeTime = 0.0f;
+
     private Vector2 _slowdownLinearVelocity = Vector2.Zero;
     private float _slowdownAngularVelocity = 0;
 
 
-    public override void _PhysicsProcess(float delta)
+    public override void _Ready()
     {
-        if (Input.IsActionJustPressed("left"))
-        {
-            Jump(Direction.Left);
-        }
+        base._Ready();
 
-        if (Input.IsActionJustPressed("right"))
-        {
-            Jump(Direction.Right);
-        }
+        GetNode<Area2D>("LoadedDiceDetector").Connect("body_entered", this, "OnBodyEntered");
     }
 
-    public override void _IntegrateForces(Physics2DDirectBodyState state)
+
+    public override void _PhysicsProcess(float delta)
     {
-        if (!_isSlowdown && Input.IsActionPressed("slow"))
+        if (Input.IsActionJustPressed("left") || Input.IsActionJustPressed("right"))
         {
             _isSlowdown = true;
-            _slowdownLinearVelocity = state.LinearVelocity;
-            _slowdownAngularVelocity = state.AngularVelocity;
+            _isChargingJump = true;
+            _jumpChargeTime = 0.0f;
         }
-        else if (_isSlowdown && !Input.IsActionPressed("slow"))
+
+        if (Input.IsActionJustReleased("left"))
         {
             _isSlowdown = false;
+            Jump(Direction.Left);
+        }
+        else if (Input.IsActionJustReleased("right"))
+        {
+            _isSlowdown = false;
+            Jump(Direction.Right);
+        }
+
+        if (_isChargingJump)
+        {
+            _jumpChargeTime += delta;
         }
 
         if (_isSlowdown)
         {
-            if (IsSlowdownFrame(++_slowdownFrameCount))
-            {
-                state.LinearVelocity = Vector2.Zero;
-                state.AngularVelocity = 0;
-            }
-            else 
-            {
-                state.LinearVelocity = _slowdownLinearVelocity;
-                state.AngularVelocity = _slowdownAngularVelocity;
-            }
+            Engine.TimeScale = 1.0f / _slowdownModulo;
         }
-
-        state.IntegrateForces();
-
-        if (!IsSlowdownFrame(_slowdownFrameCount))
+        else 
         {
-            _slowdownLinearVelocity = state.LinearVelocity;
-            _slowdownAngularVelocity = state.AngularVelocity;
+            Engine.TimeScale = 1.0f;
         }
+    }
+
+    // public override void _IntegrateForces(Physics2DDirectBodyState state)
+    // {
+    //     if (!_isSlowdown && Input.IsActionPressed("slow"))
+    //     {
+    //         _isSlowdown = true;
+    //         _slowdownLinearVelocity = state.LinearVelocity;
+    //         _slowdownAngularVelocity = state.AngularVelocity;
+    //     }
+    //     else if (_isSlowdown && !Input.IsActionPressed("slow"))
+    //     {
+    //         _isSlowdown = false;
+    //     }
+
+    //     if (_isSlowdown)
+    //     {
+    //         if (IsSlowdownFrame(++_slowdownFrameCount))
+    //         {
+    //             state.LinearVelocity = Vector2.Zero;
+    //             state.AngularVelocity = 0;
+    //         }
+    //         else 
+    //         {
+    //             state.LinearVelocity = _slowdownLinearVelocity;
+    //             state.AngularVelocity = _slowdownAngularVelocity;
+    //         }
+    //     }
+
+    //     state.IntegrateForces();
+
+    //     if (!IsSlowdownFrame(_slowdownFrameCount))
+    //     {
+    //         _slowdownLinearVelocity = state.LinearVelocity;
+    //         _slowdownAngularVelocity = state.AngularVelocity;
+    //     }
+    // }
+
+    private void OnBodyEntered(Node body)
+    {
+        if (body is LoadedDice dice)
+        {
+            dice.Kill();
+        }
+    }
+
+    internal Vector2 CalculateJumpVelocity()
+    {
+        var direction = Input.IsActionPressed("left")
+            ? Direction.Left : Input.IsActionPressed("right")
+            ? Direction.Right : Direction.None;
+
+        return JumpStrength * GetJumpDirectionVector(direction);
     }
 
     private void Stop()
@@ -78,9 +144,25 @@ public class DiceCop : RigidBody2D
     private void Jump(Direction direction)
     {
         Stop();
-        ApplyCentralImpulse(new Vector2((int)direction * 100, -50));
-        ApplyTorqueImpulse((int)direction * 360);
+        _isChargingJump = false;
+        var d = GetJumpDirectionVector(direction);
+
+        ApplyCentralImpulse(JumpStrength * d);
+        ApplyTorqueImpulse(JumpRotation * (float)direction);
     }
 
+    private Vector2 GetJumpDirectionVector(Direction direction)
+    {
+        var angle = GetJumpAngle();
+        return new Vector2((float)direction * Mathf.Cos(angle), -Mathf.Sin(angle));
+    }
+
+    private float GetJumpAngle() => 
+        (JumpAngleBounds.y - JumpAngleBounds.x) * 
+        Smooth((Mathf.Cos(2.0f * Mathf.Pi * _jumpChargeTime / JumpAngleChangeDuration - Mathf.Pi) + 1.0f) / 2.0f) + 
+        JumpAngleBounds.x;
+
     private bool IsSlowdownFrame(int frame) => frame % _slowdownModulo == 0;
+
+    private float Smooth(float t) => Mathf.Ease(t, 1.3f);
 }
---
+++ ParabolaDrawer.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+using System.Linq;
+
+public class ParabolaDrawer : Control
+{
+    private static readonly Vector2 Gravity = 
+        (Vector2) ProjectSettings.GetSetting("physics/2d/default_gravity_vector") *
+        (int) ProjectSettings.GetSetting("physics/2d/default_gravity");
+
+
+    [Export]
+    private float _duration = 1.0f;
+
+    [Export]
+    private int _resolution = 30;
+
+    [Export] 
+    private Color _color = Colors.Red;
+
+    [Export]
+    private float _width = 2.0f;
+
+
+    private DiceCop _diceCop;
+
+    public override void _Ready()
+    {
+        _diceCop = GetParent<DiceCop>();
+    }
+
+    public override void _Process(float delta)
+    {
+        Update();
+    }
+
+    public override void _Draw()
+    {
+        if (_diceCop.IsChargingJump)
+        {
+            var parabolicPoints = Enumerable
+                .Range(0, _resolution)
+                .Select(i => _duration * (float) i / (float) _resolution)
+                .Select(t => CalculateParabolicPoint(_diceCop.CalculateJumpVelocity(), t))
+                .ToArray();
+
+            DrawSetTransform(Vector2.Zero, -_diceCop.Rotation, Vector2.One);
+            DrawPolyline(parabolicPoints, _color, _width, true);
+        }
+    }
+
+    
+    private Vector2 CalculateParabolicPoint(Vector2 initialVelocity, float t)
+    {
+        var gravityTimeScale = 0.5f * t * t;
+
+        return Gravity * gravityTimeScale + initialVelocity * t;
+    }
+}
